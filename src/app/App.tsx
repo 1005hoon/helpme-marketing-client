41a28095f7bdadcaf57e99110d94f4e8786c9882
hoon <hoonnotes@naver.com>
2022-06-15 16:46:35 +0900

    add styled components and global styles

---
+++ src/app/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+	return (
+		<div>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, omnis molestias doloribus amet odit
+				rem cupiditate sint hic provident voluptatum architecto cumque quis nisi libero qui, laborum blanditiis
+				temporibus quisquam.
+			</p>
+
+			<p>안녕하세요 접니다</p>
+		</div>
+	);
+}
+
+export default App;
